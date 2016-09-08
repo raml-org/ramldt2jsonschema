8a853f67824edc0ce05bf1160258ea0ebc2189d6
Artem Kostiuk <postatum@gmail.com>
2016-09-08 10:45:22 +0300

    Create basic npm package

---
+++ examples/example.js
@@ -0,0 +1,3 @@
+var rdt2js = require("ramldt2jsonschema");
+
+rdt2js.helloWorld();
---
+++ src/index.js
@@ -0,0 +1,5 @@
+function helloWorld() {
+    console.log("Hello world")
+}
+
+module.exports.helloWorld = helloWorld;
