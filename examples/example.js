b45317546b0760b91e9958faff2b50336e19a85c
Artem Kostiuk <postatum@gmail.com>
2016-09-08 17:15:01 +0300

    Define super-complex RAML type for testing

--- examples/example.js
+++ examples/example.js
@@ -1,5 +1,8 @@
 var raml2json = require("ramldt2jsonschema");
 
 raml2json.dt2js('types.raml', 'Song', function(err, schema) {
+  if (err) {
+    console.log(err);
+  }
   console.log(JSON.stringify(schema, null, 2));
 });
--- src/dt2js_cli.js
+++ src/dt2js_cli.js
@@ -5,15 +5,22 @@ var fs = require('fs');
 
 // CLI callback that writes JSON schema to a file.
 function writeToFile(err, schema) {
+  if (err) {
+    console.log(err);
+    return;
+  }
   let pretty = JSON.stringify(schema, null, 2)
   fs.writeFileSync('schema.json', pretty);
+}
+
+// Just calls dt2js
+function dt2jsCLI(ramlFile, ramlTypeName) {
+  index.dt2js(ramlFile, ramlTypeName, writeToFile);
 }
 
 program
   .arguments('<ramlFile> <ramlTypeName>')
   .description('Convert a RAML data type into JSON schema')
-  .action(function(ramlFile, ramlTypeName){
-    index.dt2js(ramlFile, ramlTypeName, writeToFile);
-  });
+  .action(dt2jsCLI);
 
 program.parse(process.argv);
