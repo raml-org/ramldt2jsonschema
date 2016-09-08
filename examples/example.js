fbffb9396954c99887010a7a56913fb5e6c6a37f
Artem Kostiuk <postatum@gmail.com>
2016-09-08 14:06:48 +0300

    Write output of CLI to file

--- examples/example.js
+++ examples/example.js
@@ -1,5 +1,5 @@
 var raml2json = require("ramldt2jsonschema");
 
 raml2json.dt2js('types.raml', 'Song', function(err, schema) {
-  console.log(schema);
+  console.log(JSON.stringify(schema, null, 2));
 });
--- src/dt2js_cli.js
+++ src/dt2js_cli.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 var index = require('./index.js');
 var program = require('commander');
+var fs = require('fs');
 
 // CLI callback that writes JSON schema to a file.
 function writeToFile(err, schema) {
-  console.log(schema);
+  let pretty = JSON.stringify(schema, null, 2)
+  fs.writeFileSync('schema.json', pretty);
 }
 
 program
