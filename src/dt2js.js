b4f5199ed630fbf62f596a0eb82fe4b0199554a3
brevity <andrew.toelle@gmail.com>
2017-11-06 12:29:27 -0500

    build js from ast

--- src/dt2js.js
+++ src/dt2js.js
@@ -1,11 +1,11 @@
 'use strict'
 
 var yap = require('yaml-ast-parser')
-var yaml = require('js-yaml')
 var dtexp = require('datatype-expansion')
 var constants = require('./constants')
 var utils = require('./utils')
-
+var deep = require('deep-get-set')
+deep.p = true
 /**
  * Get RAML Data Types context.
  *
@@ -13,48 +13,31 @@ var utils = require('./utils')
  * @returns  {Object} - RAML data types context.
  */
 function getRAMLContext (ramlData) {
-  var yamlContent = yaml.safeLoad(ramlData)
-  var ast = yap.safeLoad(ramlData)
-  traverse(ast, () => {})
-  return yamlContent.types
+  var ast = yap.load(ramlData)
+  var jsContent = {}
+  traverse(jsContent, ast)
+  return jsContent.types
 }
-// function convertAstToJs (ast) {
-//   console.log('count', count++)
-//   console.log('key', ast)
-//   if (ast.key) {
-//     console.log('key', ast.key)
-//   }
-//   if (ast.mappings) {
-//     ast.mappings.map(convertAstToJs)
-//   }
-// }
 
-function traverse (ast, callback) {
-  // var keys = []
-  // var obj = {}
+function traverse (obj, ast, callback) {
   function recurse (keys, currentNode) {
-    // console.log('kind', currentNode.kind)
     if (currentNode.key) {
-      // console.log('key:', currentNode.key.value)
       keys = keys.concat([currentNode.key.value])
     }
     if (currentNode.value && currentNode.value.value) {
-      console.log('value: ', keys, currentNode.value.value)
-    }
-    if (currentNode.mappings) {
+      deep(obj, keys.join('.'), currentNode.value.value)
+    } else if (currentNode.value && currentNode.value.items) {
+      var values = currentNode.value.items.map(function (el) { return el.value })
+      deep(obj, keys.join('.'), values)
+    } else if (currentNode.mappings) {
       for (var i = 0; i < currentNode.mappings.length; i++) {
         recurse(keys, currentNode.mappings[i])
       }
-    }
-    if (currentNode.value && currentNode.value.mappings) {
+    } else if (currentNode.value && currentNode.value.mappings) {
       for (var o = 0; o < currentNode.value.mappings.length; o++) {
         recurse(keys, currentNode.value.mappings[o])
       }
-    } else if (currentNode.items) {
-      // console.log(currentNode)
-      recurse(keys, currentNode.value)
     }
-    // callback(currentNode)
   }
   recurse([], ast)
 }
