054d932a6b0eadfd32c1be3451ebbe9a9722b572
Jonathan Stoikovitch <jstoiko@users.noreply.github.com>
2017-12-13 11:01:37 -0800

    updating examples

--- examples/js2dt_example.js
+++ examples/js2dt_example.js
@@ -1,13 +1,13 @@
 'use strict'
 
-var raml2json = require('ramldt2jsonschema')
+var r2j = require('ramldt2jsonschema')
 var join = require('path').join
 var fs = require('fs')
 
-var filePath = join(__dirname, '../test/examples/json/simple_person.json')
+var filePath = join(__dirname, '../test/integration/json/complex_cat.json')
 var jsonData = fs.readFileSync(filePath).toString()
 
-raml2json.js2dt(jsonData, 'Person', function (err, raml) {
+r2j.js2dt(jsonData, 'Person', function (err, raml) {
   if (err) {
     console.log(err)
     return
