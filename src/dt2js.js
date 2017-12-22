9cc13e377100e1b669d1604b6261d2aba2bf1004
brevity <andrew.toelle@gmail.com>
2017-12-22 13:56:35 -0500

    add jsDoc for convertPatternProperties()

--- src/dt2js.js
+++ src/dt2js.js
@@ -253,6 +253,24 @@ function convertDateType (data) {
 }
 
 /**
+ * Change RAML pattern properties to JSON patternProperties.
+ *
+ * @param  {Object} data - the library fragment to convert
+ * @returns  {Object}
+ */
+function convertPatternProperties (data) {
+  Object.keys(data.properties).map(function (key) {
+    if (/^\/.*\/$/.test(key)) {
+      data.patternProperties = data.patternProperties || {}
+      var stringRegex = key.slice(1, -1)
+      data.patternProperties[stringRegex] = data.properties[key]
+      delete data.properties[key]
+    }
+  })
+  return data
+}
+
+/**
  * Change RAML displayName to JSON schema title.
  *
  * @param  {Object} data
@@ -332,17 +350,6 @@ function schemaForm (data, reqStack, prop) {
   if (data.properties) {
     convertPatternProperties(data)
   }
-  return data
-}
-function convertPatternProperties (data) {
-  Object.keys(data.properties).map(function (key) {
-    if (/^\/.*\/$/.test(key)) {
-      data.patternProperties = data.patternProperties || {}
-      var stringRegex = key.slice(1, -1)
-      data.patternProperties[stringRegex] = data.properties[key]
-      delete data.properties[key]
-    }
-  })
   return data
 }
 
