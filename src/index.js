ff39d6249d8b95d326a81f5d012bc0436fc70d6b
Carlos Crespo Milán <kace91@gmail.com>
2018-05-07 15:46:56 +0200

    fix issue #48 - modified to make it non-breaking

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@
 var dt2js = require('./dt2js')
 var js2dt = require('./js2dt')
 
-module.exports.dt2js = dt2js
+module.exports.dt2js = dt2js.dt2js
+module.exports.dt2js.setBasePath = dt2js.setBasePath
 module.exports.js2dt = js2dt.js2dt
