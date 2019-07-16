8405d245e702be3627b0e85b969891c2032ffc4b
Artem Kostiuk <postatum@gmail.com>
2019-07-16 12:36:25 +0300

    Rework examples

--- examples/js2dt_example.js
+++ examples/js2dt_example.js
@@ -1,17 +1,22 @@
 'use strict'
 
-var r2j = require('ramldt2jsonschema')
-var join = require('path').join
-var fs = require('fs')
+const r2j = require('ramldt2jsonschema')
+const join = require('path').join
+const fs = require('fs')
+const yaml = require('js-yaml')
 
-var filePath = join(__dirname, '../test/integration/json/complex_cat.json')
-var jsonData = fs.readFileSync(filePath).toString()
+const filePath = join(__dirname, '../test/integration/json/complex_cat.json')
+const jsonData = fs.readFileSync(filePath).toString()
 
-r2j.js2dt(jsonData, 'Person', function (err, raml) {
-  if (err) {
+async function main () {
+  try {
+    const raml = await r2j.js2dt(jsonData, 'Cat')
+  } catch (err) {
     console.log(err)
     return
   }
   console.log('#%RAML 1.0 Library\n')
-  console.log(raml)
-})
+  console.log(yaml.safeDump(raml, { 'noRefs': true }))
+}
+
+main()
