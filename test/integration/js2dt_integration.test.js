9000682ecee7728793becad608b99e535a474e19
Artem Kostiuk <postatum@gmail.com>
2019-07-22 13:32:41 +0300

    Fix js2dt integration tests

--- test/integration/js2dt_integration.test.js
+++ test/integration/js2dt_integration.test.js
@@ -16,44 +16,32 @@
  */
 
 const path = require('path')
-const parser = require('raml-1-parser')
+const wap = require('webapi-parser').WebApiParser
 
 const helpers = require('../helpers')
 const js2dt = require('../../src/js2dt')
-const cli = require('../../src/js2dt_cli')
+const js2dtCLI = require('../../src/js2dt_cli')
 
 const EXAMPLES_FOLDER = path.join(__dirname, 'json')
 
 /**
- * Log RAML validation error.
+ * Test file by running js2dt script on it and then validating
+ * with webapi-parser.
  */
-function logValidationError (error) {
-  console.log('FAIL (RAML validation):')
-  error.parserErrors.forEach(function (el) {
-    const errMessage = '- ' + el.message + ' [' +
-      el.range.start.line + '.' + el.range.start.column + ':' +
-      el.range.end.line + '.' + el.range.end.column + ']'
-    console.log(errMessage)
+async function defineTests () {
+  describe('js2dt integration test', () => {
+    helpers.getFiles(EXAMPLES_FOLDER).forEach(filepath => {
+      it(`should convert ${filepath}`, async () => {
+        const ramlStr = await js2dtCLI(filepath, 'TestType')
+        const model = await wap.raml10.parse(ramlStr)
+        const report = await wap.raml10.validate(model)
+        if (!report.conforms) {
+          console.log(ramlStr)
+          throw new Error(report.toString())
+        }
+      })
+    })
   })
 }
 
-describe('js2dt integration test', () => {
-  helpers.forEachFileIn(EXAMPLES_FOLDER, (filepath) => {
-    /**
-     * Test file by running js2dt script on it and then validating
-     * with raml-1-parser.
-     */
-    it(`should convert ${filepath}`, () => {
-      const typeName = 'TestType'
-      js2dt.setBasePath(EXAMPLES_FOLDER)
-      const raml = cli(filepath, typeName)
-
-      try {
-        parser.parseRAMLSync(raml, { 'rejectOnErrors': true })
-      } catch (error) {
-        logValidationError(error)
-        throw error
-      }
-    })
-  })
-})
+defineTests()
